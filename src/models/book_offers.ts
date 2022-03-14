cb9e40b355d24342b8d7195d66bc2246fd00a283
Ildar Gumirov <ildaruz@users.noreply.github.com>
2022-03-14 15:52:19 +0100

    book offers and refactoring (#12)

---
+++ src/models/book_offers.ts
@@ -0,0 +1,57 @@
+import * as _ from "lodash";
+import BigNumber from "bignumber.js";
+import { parseOrderbookOrder } from "../v1/ledger/parse/orderbook-order";
+import { Issue } from "../v1/common/types/objects/amounts";
+
+export type OrderbookInfo = {
+  base: Issue;
+  counter: Issue;
+};
+
+function isSameIssue(a: Issue, b: Issue) {
+  return a.currency === b.currency && a.counterparty === b.counterparty;
+}
+
+function directionFilter(direction: string, order: any) {
+  return order.specification.direction === direction;
+}
+
+function flipOrder(order: any) {
+  const specification = order.specification;
+  const flippedSpecification = {
+    quantity: specification.totalPrice,
+    totalPrice: specification.quantity,
+    direction: specification.direction === "buy" ? "sell" : "buy",
+  };
+  const newSpecification = _.merge({}, specification, flippedSpecification);
+  return _.merge({}, order, { specification: newSpecification });
+}
+
+function alignOrder(base: Issue, order: any) {
+  const quantity = order.specification.quantity;
+  return isSameIssue(quantity, base) ? order : flipOrder(order);
+}
+
+export function formatBidsAndAsks(orderbook: OrderbookInfo, offers: any[]) {
+  // the "base" currency is the currency that you are buying or selling
+  // the "counter" is the currency that the "base" is priced in
+  // a "bid"/"ask" is an order to buy/sell the base, respectively
+  // for bids: takerGets = totalPrice = counter, takerPays = quantity = base
+  // for asks: takerGets = quantity = base, takerPays = totalPrice = counter
+  // quality = takerPays / takerGets; price = totalPrice / quantity
+  // for bids: lowest quality => lowest quantity/totalPrice => highest price
+  // for asks: lowest quality => lowest totalPrice/quantity => lowest price
+  // for both bids and asks, lowest quality is closest to mid-market
+  // we sort the orders so that earlier orders are closer to mid-market
+  const orders = offers
+    .sort((a, b) => {
+      return new BigNumber(a.quality).comparedTo(b.quality);
+    })
+    .map(parseOrderbookOrder);
+
+  const alignedOrders = orders.map(_.partial(alignOrder, orderbook.base));
+  const bids = alignedOrders.filter(_.partial(directionFilter, "buy"));
+  const asks = alignedOrders.filter(_.partial(directionFilter, "sell"));
+
+  return { bids, asks };
+}
---
+++ test/client/book_offers.test.ts
@@ -0,0 +1,117 @@
+import nconf from "nconf";
+import { expect } from "chai";
+import { Client } from "../../src/index";
+
+describe("Client", () => {
+  before(async function () {
+    Client.setup(nconf.get("xrpl:connections:mainnet"));
+    await Client.connect();
+  });
+
+  describe("getBookOffers", () => {
+    it("returns offers", async function () {
+      const taker = "rsuUjfWxrACCAwGQDsNeZUhpzXf1n1NK5Z";
+      const takerGets = {
+        currency: "USD",
+        issuer: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+      };
+      const takerPays = {
+        currency: "BTC",
+        issuer: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+      };
+
+      const result: any = await Client.getBookOffers(taker, takerGets, takerPays);
+      const offer = result.offers[0];
+
+      expect(Object.keys(offer)).to.eql([
+        "Account",
+        "BookDirectory",
+        "BookNode",
+        "Flags",
+        "LedgerEntryType",
+        "OwnerNode",
+        "PreviousTxnID",
+        "PreviousTxnLgrSeq",
+        "Sequence",
+        "TakerGets",
+        "TakerPays",
+        "index",
+        "owner_funds",
+        "quality",
+      ]);
+    });
+
+    it("returns offers with limit", async function () {
+      const taker = "rsuUjfWxrACCAwGQDsNeZUhpzXf1n1NK5Z";
+      const takerGets = {
+        currency: "USD",
+        issuer: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+      };
+      const takerPays = {
+        currency: "BTC",
+        issuer: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+      };
+
+      const result: any = await Client.getBookOffers(taker, takerGets, takerPays, { limit: 1 });
+
+      delete result._nodepref;
+      delete result.warnings;
+      expect(Object.keys(result)).to.eql(["ledger_hash", "ledger_index", "offers", "validated"]);
+      expect(result.offers.length).to.eq(1);
+    });
+  });
+
+  describe("getOrderbook", () => {
+    it("returns offers", async function () {
+      const taker = "rsuUjfWxrACCAwGQDsNeZUhpzXf1n1NK5Z";
+      const orderbook = {
+        base: {
+          currency: "USD",
+          counterparty: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+        },
+        counter: {
+          currency: "BTC",
+          counterparty: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+        },
+      };
+
+      const result: any = await Client.getOrderbook(taker, orderbook);
+      expect(Object.keys(result)).to.eql(["bids", "asks"]);
+      expect(Object.keys(result.bids[0])).to.eql(["specification", "properties", "data"]);
+      expect(Object.keys(result.bids[0].data)).to.eql([
+        "Account",
+        "BookDirectory",
+        "BookNode",
+        "Flags",
+        "LedgerEntryType",
+        "OwnerNode",
+        "PreviousTxnID",
+        "PreviousTxnLgrSeq",
+        "Sequence",
+        "TakerGets",
+        "TakerPays",
+        "index",
+        "owner_funds",
+        "quality",
+      ]);
+
+      expect(Object.keys(result.asks[0])).to.eql(["specification", "properties", "data"]);
+      expect(Object.keys(result.asks[0].data)).to.eql([
+        "Account",
+        "BookDirectory",
+        "BookNode",
+        "Flags",
+        "LedgerEntryType",
+        "OwnerNode",
+        "PreviousTxnID",
+        "PreviousTxnLgrSeq",
+        "Sequence",
+        "TakerGets",
+        "TakerPays",
+        "index",
+        "owner_funds",
+        "quality",
+      ]);
+    });
+  });
+});
