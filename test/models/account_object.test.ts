a26628b0831fa3820d9a8b5c7d618c346493ebdf
Ildar Gumirov <ildar.gumirov@gmail.com>
2022-04-17 22:28:37 +0200

    accountObjectsToAccountLines tests

--- test/models/account_object.test.ts
+++ test/models/account_object.test.ts
@@ -19,7 +19,7 @@ describe("Models", () => {
       ]);
     });
 
-    it("returns account lines", function () {
+    it("returns account lines with balances", function () {
       const objects = require("../examples/responses/objects/rhuCDThQpvQ6inFv1KguGymhXBK7K2So9m.json");
       const result: any = Models.accountObjectsToAccountLines("rhuCDThQpvQ6inFv1KguGymhXBK7K2So9m", objects);
       expect(result).to.eql([
@@ -100,6 +100,131 @@ describe("Models", () => {
           balance: "0",
           currency: "USD",
           limit: "1000000000",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+      ]);
+    });
+
+    it("returns all account lines", function () {
+      const objects = require("../examples/responses/objects/rQHqfA5bbxgPbBQajcRBBJSfuKLMDW17nr.json");
+      const result: any = Models.accountObjectsToAccountLines("rQHqfA5bbxgPbBQajcRBBJSfuKLMDW17nr", objects);
+      expect(result.length).to.eq(13)
+      expect(result).to.eql([
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "8136181634625030e1",
+          currency: "01434E5900000000C180694BFF0A625D00000000",
+          limit: "8136124662444204e2",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "015841550000000041F76FF6ECB0BAC600000000",
+          limit: "928316338.5518048",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rGiWhwhtGNdu4Df4ZB68HzQNuxTTy28B5V",
+          balance: "0",
+          currency: "015553440000000041B096111BF28F7D00000000",
+          limit: "18255754842.13878",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: true,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "0158415500000000C1F76FF6ECB0BAC600000000",
+          limit: "1077237752426209e7",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E590000000041576FF6ECB0BAC500000000",
+          limit: "0",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: true,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E5900000000C1E76FF6ECB0BAC600000000",
+          limit: "1160422227.776528",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E5900000000C1F26FF6ECB0BAC600000000",
+          limit: "1099185549769381e-2",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E590000000041F76FF6ECB0BAC600000000",
+          limit: "928316432.8380369",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: true,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "1077246080380300e-1",
+          currency: "0158525000000000C1F76FF6ECB0BAC600000000",
+          limit: "1077257977517231e3",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E5900000000C1B08FF6ECB0BAC500000000",
+          limit: "538782896.8579473",
+          limit_peer: "0",
+          no_ripple: true,
+          no_ripple_peer: false,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E5900000000C1B05FF6ECB0BAC500000000",
+          limit: "0",
+          limit_peer: "301669700.0912015",
+          no_ripple: true,
+          no_ripple_peer: true,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E590000000041B08FF6ECB0BAC500000000",
+          limit: "18561.33771773405",
+          limit_peer: "3445.229131879006",
+          no_ripple: true,
+          no_ripple_peer: true,
+        },
+        {
+          account: "rEFMsxytphWCgfTdAXBBYvwhnB5GWjBrY5",
+          balance: "0",
+          currency: "01434E5900000000C1F66FF6ECB0BAC600000000",
+          limit: "1080809366.120872",
           limit_peer: "0",
           no_ripple: true,
           no_ripple_peer: false,
