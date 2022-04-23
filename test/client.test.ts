786d79ae824c8ea66d7a26aefa9b79adc24149ab
Ildar Gumirov <ildar.gumirov@gmail.com>
2022-04-23 11:37:35 +0200

    Streams subscriptions

---
+++ test/client.test.ts
@@ -0,0 +1,41 @@
+import nconf from "nconf";
+import { expect } from "chai";
+import { Client } from "../src/index";
+import { Connection } from "../src/connection";
+
+describe("Client", () => {
+  describe("mainnet", () => {
+    before(async function () {
+      this.timeout(15000);
+      Client.setup(nconf.get("xrpl:connections:mainnet"));
+      await Client.connect();
+    });
+
+    describe("findConnection", () => {
+      it("returns connection", async function () {
+        expect(Client.findConnection()).to.be.an.instanceOf(Connection);
+      });
+
+      it("returns connection with by type history", async function () {
+        const connection = Client.findConnection("history") as Connection;
+        expect(connection.types).to.include("history");
+      });
+
+      it("returns connection with by type gateway_balances", async function () {
+        const connection = Client.findConnection("gateway_balances") as Connection;
+        expect(connection.url).to.eq("wss://xrplcluster.com");
+        expect(connection.types).to.include("gateway_balances");
+      });
+
+      it("returns connection with by url", async function () {
+        const connection = Client.findConnection(undefined, "wss://s1.ripple.com") as Connection;
+        expect(connection.url).to.eq("wss://s1.ripple.com");
+      });
+
+      it("returns connection with by url and strong filter ", async function () {
+        const connection = Client.findConnection(undefined, "wss://test.com", true);
+        expect(connection).to.eq(undefined);
+      });
+    });
+  });
+});
---
+++ test/connection.test.ts
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import nconf from "nconf";
+// import { expect } from "chai";
+
+import { Connection } from "../src/connection";
+
+let connection: Connection;
+
+describe("Connection", () => {
+  beforeEach(async function () {
+    this.timeout(15000);
+    const server = nconf.get("xrpl:connections:mainnet")[0];
+    connection = new Connection(server.url, server.type);
+    await connection.connect();
+  });
+
+  afterEach(async function () {
+    this.timeout(15000);
+    await connection.disconnect();
+  });
+
+  describe("request", () => {
+    it("can receive ledger", function (done) {
+      this.timeout(15000);
+      expect(connection.streams).to.eql({ ledger: 1 });
+
+      connection.once("ledgerClosed", (ledgerStream) => {
+        expect(ledgerStream.type).to.eq("ledgerClosed");
+        done();
+      });
+    });
+
+    it("can subscribe and unsubscribe", async function () {
+      expect(connection.streams).to.eql({ ledger: 1 });
+
+      await connection.request({
+        command: "subscribe",
+        streams: ["transactions"],
+      });
+
+      expect(connection.streams).to.eql({ ledger: 1, transactions: 1 });
+
+      await connection.request({
+        command: "subscribe",
+        streams: ["transactions"],
+      });
+
+      expect(connection.streams).to.eql({ ledger: 1, transactions: 2 });
+
+      await connection.request({
+        command: "unsubscribe",
+        streams: ["transactions"],
+      });
+
+      expect(connection.streams).to.eql({ ledger: 1, transactions: 1 });
+
+      await connection.request({
+        command: "unsubscribe",
+        streams: ["transactions"],
+      });
+
+      expect(connection.streams).to.eql({ ledger: 1 });
+    });
+
+    it("can subscribe and receive transaction", function (done) {
+      this.timeout(15000);
+      expect(connection.streams).to.eql({ ledger: 1 });
+
+      connection.request({
+        command: "subscribe",
+        streams: ["transactions"],
+      });
+
+      expect(connection.streams).to.eql({ ledger: 1, transactions: 1 });
+
+      connection.once("transaction", (transactionStream) => {
+        expect(transactionStream.type).to.eq("transaction");
+        done();
+      });
+    });
+  });
+});
