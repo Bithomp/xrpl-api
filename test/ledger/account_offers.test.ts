77b368d2624d121f51a19a7ce594b6e0afe51c25
Ildar Gumirov <ildar.gumirov@gmail.com>
2022-04-25 12:02:51 +0200

    Update account_offers.test.ts

--- test/ledger/account_offers.test.ts
+++ test/ledger/account_offers.test.ts
@@ -14,7 +14,7 @@ describe("Client", () => {
       const result: any = await Client.getAccountOffers("rsuUjfWxrACCAwGQDsNeZUhpzXf1n1NK5Z");
       const offer = result.offers[0];
 
-      expect(Object.keys(offer)).to.eql(["flags", "quality", "seq", "taker_gets", "taker_pays"]);
+      expect(Object.keys(offer).sort()).to.eql(["flags", "quality", "seq", "taker_gets", "taker_pays"]);
       expect(Object.keys(offer.taker_gets)).to.eql(["currency", "issuer", "value"]);
       expect(Object.keys(offer.taker_pays)).to.eql(["currency", "issuer", "value"]);
     });
