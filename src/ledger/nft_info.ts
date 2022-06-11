ca64089fdbb82870511da817c7fdc058c9e14a69
Ildar Gumirov <ildaruz@users.noreply.github.com>
2022-06-11 18:26:18 +0200

    getNftInfo with clio (#26)

---
+++ src/ledger/nft_info.ts
@@ -0,0 +1,40 @@
+import * as Client from "../client";
+
+// tslint:disable-next-line:no-empty-interface
+export interface GetNftInfoOptions {}
+
+/**
+ * @returns {string | null}
+ * @exception {Error}
+ */
+// tslint:disable-next-line:variable-name
+export async function getNftInfo(nft_id: string, options: GetNftInfoOptions = {}): Promise<object | null> {
+  // strong search only Clio servers support the command
+  const connection: any = Client.findConnection("clio", undefined, true);
+  if (!connection) {
+    throw new Error("There is no connection");
+  }
+
+  const response: any = await connection.request({
+    command: "nft_info",
+    nft_id,
+  });
+
+  if (!response) {
+    return null;
+  }
+
+  if (response.error) {
+    const { error, error_code, error_message, status, validated } = response;
+
+    return {
+      error,
+      error_code,
+      error_message,
+      status,
+      validated,
+    };
+  }
+
+  return response?.result;
+}
