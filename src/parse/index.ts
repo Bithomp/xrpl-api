64c7736aeb875e014a431bc046b38cb6f0120291
Ildar Gumirov <ildar.gumirov@gmail.com>
2024-11-02 21:33:52 +0100

    export Outcomes functions

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -1,4 +1,5 @@
 export * as Ledger from "./ledger";
 export * as Utils from "./utils";
+export * as Outcomes from "./outcome/index";
 export { parseOutcome } from "./outcome";
 export { parseTransactionType, parseTransaction } from "./transaction";
